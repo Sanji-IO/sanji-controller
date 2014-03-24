361a7016f247aa1ad886ac80d725bbfbe4f728cf
simon.wang <simon.wang@moxa.com>
2014-03-24 16:28:55 +0800

    [Sanji Controller]
    [what] Add VERBOSE in DEBUG_PRINT()

--- debug.h
+++ debug.h
@@ -2,8 +2,10 @@
 #define _DEBUG_H_
 
 /* debug macro */
-#ifdef DEBUG
-#define DEBUG_PRINT(fmt, ...) do { fprintf(stderr, "[sanji] %s: %s: %d: " fmt "\n", __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__); } while (0)
+#if defined DEBUG
+#define DEBUG_PRINT(fmt, ...) do { fprintf(stderr, "[SANJI] %s: %s: %d: " fmt "\n", __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__); } while (0)
+#elif defined VERBOSE
+#define DEBUG_PRINT(fmt, ...) do { fprintf(stderr, "[SANJI] " fmt "\n", ##__VA_ARGS__); } while (0)
 #else
 #define DEBUG_PRINT(fmt, ...)
 #endif
