4ad5d389cf197106830e34fb94a586a698ec6f1b
simon.wang <simon.wang@moxa.com>
2014-06-05 11:08:00 +0800

    [model test]
    Refactor code and naming.

---
+++ model_test/ethernet_testcase_1.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+source ../functional_test/testcase_config
+MQTT_SERVER_IP=127.0.0.1
+
+###################################################
+# Test Items:
+###################################################
+
+# create
+${PUB_TOOL} -t "${TOPIC_CONTROLLER}" -h ${MQTT_SERVER_IP} -f ${JSON_CONTEXT}/ethernet/request_create_ethernet.json
+sleep 3
+
+# read capability
+${PUB_TOOL} -t "${TOPIC_CONTROLLER}" -h ${MQTT_SERVER_IP} -f ${JSON_CONTEXT}/ethernet/request_readcap_ethernet.json
+sleep 3
+
+# read
+${PUB_TOOL} -t "${TOPIC_CONTROLLER}" -h ${MQTT_SERVER_IP} -f ${JSON_CONTEXT}/ethernet/request_read_ethernet.json
+sleep 3
+
+# update
+${PUB_TOOL} -t "${TOPIC_CONTROLLER}" -h ${MQTT_SERVER_IP} -f ${JSON_CONTEXT}/ethernet/request_update_ethernet.json
+sleep 3
+
+# delete
+${PUB_TOOL} -t "${TOPIC_CONTROLLER}" -h ${MQTT_SERVER_IP} -f ${JSON_CONTEXT}/ethernet/request_delete_ethernet.json
+sleep 3
---
+++ model_test/model_testcase_1.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+source ../functional_test/testcase_config
+MQTT_SERVER_IP=127.0.0.1
+
+###################################################
+# Test Items:
+###################################################
+
+# create
+${PUB_TOOL} -t "${TOPIC_CONTROLLER}" -h ${MQTT_SERVER_IP} -f ${JSON_CONTEXT}/model/request_create_model.json
+sleep 3
+
+# read capability
+${PUB_TOOL} -t "${TOPIC_CONTROLLER}" -h ${MQTT_SERVER_IP} -f ${JSON_CONTEXT}/model/request_readcap_model.json
+sleep 3
+
+# read
+${PUB_TOOL} -t "${TOPIC_CONTROLLER}" -h ${MQTT_SERVER_IP} -f ${JSON_CONTEXT}/model/request_read_model.json
+sleep 3
+
+# update
+${PUB_TOOL} -t "${TOPIC_CONTROLLER}" -h ${MQTT_SERVER_IP} -f ${JSON_CONTEXT}/model/request_update_model.json
+sleep 3
+
+# delete
+${PUB_TOOL} -t "${TOPIC_CONTROLLER}" -h ${MQTT_SERVER_IP} -f ${JSON_CONTEXT}/model/request_delete_model.json
+sleep 3
+
+# other method
+${PUB_TOOL} -t "${TOPIC_CONTROLLER}" -h ${MQTT_SERVER_IP} -f ${JSON_CONTEXT}/model/request_other_model.json
+sleep 3
+
+# response procedure
+${PUB_TOOL} -t "${TOPIC_CONTROLLER}" -h ${MQTT_SERVER_IP} -f ${JSON_CONTEXT}/model/response_read_model.json
+sleep 3
